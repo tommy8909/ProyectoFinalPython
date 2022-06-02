286a96bffec39824b623f8c6d0ab0ea59d204714
AntonioDelGreco <ddiegoantonio1@gmail.com>
2022-06-02 00:30:22 -0300

    1

---
+++ py.py
@@ -0,0 +1,5 @@
+#BIBLIOTECA VIRTUAL
+""" agregar libro, eliminar libro, caracteristicas del libro, sucursales del libro, mostrar todos los libros,
+busqueda de libros, menu, un inicio de sesion, diferencia de libros para cada usuario, libros reservados para cada uno,
+"""
+
